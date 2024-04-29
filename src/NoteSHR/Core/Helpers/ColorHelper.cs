f9cfa40fa422b3b7ca1c14fd68d5162b8a774f18
MRmlik12 <contact@dolczyk.rocks>
2024-04-29 21:42:59 +0200

    Add colors to note header

---
+++ src/NoteSHR/Core/Helpers/ColorHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NoteSHR.Core.Helpers;
+
+public static class ColorHelper
+{
+    private static readonly string[] NoteColors = { "0F528C", "9ADE86", "FF6162", "FFC000", "47729A", "7D64AD" };
+    
+    public static string GenerateColor()
+    {
+        var random = new Random();
+        var color = $"#{NoteColors[random.Next(0, NoteColors.Length - 1)]}";
+        
+        return color;
+    }
+}
--- src/NoteSHR/Core/Models/Note.cs
+++ src/NoteSHR/Core/Models/Note.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using NoteSHR.ViewModels;
+using ColorHelper = NoteSHR.Core.Helpers.ColorHelper;
 
 namespace NoteSHR.Core.Models;
 
@@ -9,7 +10,9 @@ public class Note(double x, double y) : INotifyPropertyChanged
 {
     private ObservableCollection<NodeViewModel> _nodes = new ();
     private double _width = 200.0d;
-    
+    private string _headerColor = ColorHelper.GenerateColor();
+    private string _backgroundColor = "#222222"; 
+
     public Guid Id { get; } = Guid.NewGuid();
 
     public double X
@@ -48,6 +51,25 @@ public class Note(double x, double y) : INotifyPropertyChanged
         private set
         {
             _nodes = value;
+        }
+    }
+
+    public string HeaderColor
+    {
+        get => _headerColor;
+        private set
+        {
+            _headerColor = value;
+            OnPropertyChanged(nameof(HeaderColor));
+        }
+    }
+    public string BackgroundColor
+    {
+        get => _backgroundColor;
+        private set
+        {
+            _backgroundColor = value;
+            OnPropertyChanged(nameof(BackgroundColor));
         }
     }
 
