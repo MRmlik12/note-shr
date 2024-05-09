132a6f6f215a38f77f846c194fb3e36d1513c4e0
MRmlik12 <contact@dolczyk.rocks>
2024-05-09 22:44:42 +0200

    Add color picker to change header color of note

---
+++ src/NoteSHR/Core/Converters/ColorToHexConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace NoteSHR.Core.Converters;
+
+public class ColorToHexConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return Color.Parse((string)value);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var color = (Color)value;
+        
+        return color.ToString();
+    }
+}
